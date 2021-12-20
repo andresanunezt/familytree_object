e49b409ddc24973e31345ec7c48ed2639ef8b0dc
Andrés Nuñez Tinajero <77559024+andresanunezt@users.noreply.github.com>
2021-12-20 17:19:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const User = require('./index') 
 
-describe('User class', () => { 
-
-const me = new User('Andrés Alejandro Nuñez Tinajero',['Karina Tinajero Rosario', 'Andrés Nuñez Betancourt'],[])
 
 class User {
 
-    constructor(name,parents,children){
-    
-    this.name = name;
-    this.parents = parents;
-    this.children = children;
-    
-    }
-    
-    childOf() {
-    
-      return `${this.name} is a child of ${this.parents[0]} and ${this.parents[1]}`
-      
-    }
-    
-    parentOf(){
-      return `${this.name} is a parent to  ${this.children.name} `
-    
-    }
-    
+constructor(name,parents,children){
 
-    }
+this.name = name;
+this.parents = parents;
+this.children = children;
 
-    module.exports = User
+}
 
- console.log(me, mom, dad)
- console.log(me.childOf())
- console.log(dad.childOf(),"and" )
- console.log(mom.parentOf())
-	
-console.log(me, mom, dad)	
+childOf() {
+
+  return `${this.name} is a child of ${this.parents[0]} and ${this.parents[1]}`
+  
+}
+
+parentOf(){
+  return `${this.name} is a parent to  ${this.children.name} `
+
+}
+
+
+}
+
+
+
+
+
+
+const me = new User('Andrés Alejandro Nuñez Tinajero',['Karina Tinajero Rosario', 'Andrés Nuñez Betancourt'])
+
+const mom = new User('Karina Tinajero Rosario',['Aida Rosario Rosario', 'Andrés Tinajero Castro'], me)
+
+const dad = new User('Andrés Nuñez Betancourt',['Basilia Betancourt', 'Andrés Nuñez'], me)
+
+console.log(me, mom, dad)
 console.log(me.childOf())
 console.log(dad.childOf(),"and" )
 console.log(mom.parentOf())
